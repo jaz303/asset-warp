fb8050c9098b378a31c063f981fdddd9cc128765
Jason Frame <jason@magiclamp.co.uk>
2010-04-20 23:22:40 +0100

    added mime helpers

---
+++ lib/asset_warp/mime.rb
@@ -0,0 +1,29 @@
+module AssetWarp
+  module MIME
+    WEB_SAFE_IMAGE_MAP = {
+      'image/jpeg'  => 'jpg',
+      'image/pjpeg' => 'jpg',
+      'image/gif'   => 'gif',
+      'image/png'   => 'png',
+    }.freeze
+    
+    PDF = 'application/pdf'
+    
+    CONTENT_CLASSES = {
+      :pdf => [PDF],
+      :web_safe_image => WEB_SAFE_IMAGE_MAP.keys
+    }
+    
+    def self.expand_content_class(symbol)
+      CONTENT_CLASSES[symbol] || []
+    end
+    
+    def self.pdf?(content_type)
+      PDF == content_type
+    end
+    
+    def self.web_safe_image?(content_type)
+      WEB_SAFE_IMAGE_MAP.key?(content_type)
+    end
+  end
+end
